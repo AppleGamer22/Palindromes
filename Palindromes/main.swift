6dd3b1b1bd83d7419357bc5bda271d8cd59e6beb
AppleGamer22 <applegamer22@icloud.com>
2017-11-26 16:13:25 +1030

    Initial Commit

---
+++ Palindromes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Palindromes
+//
+//  Created by Omri Bornstein on 26/11/17.
+//  Copyright © 2017 AppleGamer22 Software Development. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
